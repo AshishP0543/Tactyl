feae501446545ccf79898a7fcf220a82e94b0389
AshishP0543 <ashish.patil@flamapp.com>
2025-07-26 18:40:23 +0530

    Initial Commit

---
+++ Tactyl/TactylApp.swift
@@ -0,0 +1,17 @@
+//
+//  TactylApp.swift
+//  Tactyl
+//
+//  Created by Ashish Patil on 26/07/25.
+//
+
+import SwiftUI
+
+@main
+struct TactylApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TactylTests/TactylTests.swift
@@ -0,0 +1,17 @@
+//
+//  TactylTests.swift
+//  TactylTests
+//
+//  Created by Ashish Patil on 26/07/25.
+//
+
+import Testing
+@testable import Tactyl
+
+struct TactylTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
